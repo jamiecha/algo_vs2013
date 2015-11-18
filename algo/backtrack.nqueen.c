f6278b8ab4fedba192429b4ee1e63807cdaa5168
jamiecha <jamiecha@gmail.com>
2015-11-18 13:49:58 +0900

    improve nqueen, add time util

--- algo/backtrack.nqueen.c
+++ algo/backtrack.nqueen.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+extern double now(void);
+extern double took(double a, double b);
 int br;
 
-#define NMAX 15
 
+#define NMAX 14
 int board[NMAX][NMAX];
 int ans = 0;
 
@@ -34,7 +36,7 @@ void init_board(int n){
 }
 
 
-int is_safe(int row, int col, int n){
+int is_safe1(int row, int col, int n){
 	int x,y;
 
 	// the first row
@@ -65,6 +67,46 @@ int is_safe(int row, int col, int n){
 }
 
 
+int is_safe2(int row, int col, int n){
+	int x,y;
+
+	// the first row
+	if(row==0){
+		return 1;
+	}
+
+	// column collision
+	for(y=0;y<row;y++){
+		if(board[y][col]){
+			return 0;
+		}
+	}
+
+	// diagonal left-back collision
+	x=col-1; 
+	y=row-1;
+	while( x>=0 && y>=0 ){
+		if(board[y][x])
+			return 0;
+		x--;y--;
+	}
+
+	// diagonal right-back collision
+	x=col+1;
+	y=row-1;
+	while( x<n && y>=0 ){
+		if(board[y][x])
+			return 0;
+		x++;y--;
+	}
+
+	// everything is okay
+	return 1;
+
+}
+
+
+
 void nqueen(int row, int n){
 
 	int col;
@@ -77,7 +119,7 @@ void nqueen(int row, int n){
 	}
 
 	for(col=0;col<n;col++){
-		if(is_safe(row,col,n)){
+		if(is_safe2(row,col,n)){      // compare safe1 & safe2
 			board[row][col] = 1;
 			nqueen(row+1,n);
 			board[row][col] = 0;
@@ -91,11 +133,14 @@ void nqueen(int row, int n){
 
 void backtrack_nqueen(void){
 	int n;
+	double s,e;
 	for(n=4;n<=NMAX;n++){
+		s = now();
 		init_board(n);
 		ans = 0;
 		nqueen(0,n);
-		printf("%d %d\n",n,ans);
+		e = now();
+		printf("%d %d took=%f\n",n,ans,took(s,e));
 	}
 
 	br = 1;
---
+++ algo/util.time.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <time.h>
+
+double now(void){
+	time_t t;
+	t = clock();
+	return (double)t;
+}
+
+
+double took(double a, double b){
+	return (double)((b-a)/CLOCKS_PER_SEC);
+}
